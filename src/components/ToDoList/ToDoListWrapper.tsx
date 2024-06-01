55a2eb212ae95c250354d439ed6da7f9d88f12c7
Zhasulan Muratov <zasulanmuratov52@gmail.com>
2024-06-01 14:27:40 +0500

    main logic is done

--- src/components/ToDoList/ToDoListWrapper.tsx
+++ src/components/ToDoList/ToDoListWrapper.tsx
@@ -1,15 +1,14 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 
-import "../../styles/dashboard/ToDoList.scss";
+import "../../styles/root/ToDoList.scss";
 import { TrashIcon } from "../Icons/TrashIcon";
 import { SearchText } from "./SearchText";
+import { AddNewItem } from "./AddNewItem";
 
 export const ToDoList = () => {
   const [activeIndexes, setActiveIndexes] = useState<number[]>([]);
   const [items, setItems] = useState<string[]>(["Swimming pool"]);
   const [searchInput, setSearchInput] = useState<string>("");
-
-  const inputRefAddNewList = useRef<HTMLInputElement>(null);
 
   const handleClickToggleClass = (index: number) => {
     setActiveIndexes((prevIndexes) =>
@@ -17,12 +16,6 @@ export const ToDoList = () => {
         ? prevIndexes.filter((i) => i !== index)
         : [...prevIndexes, index]
     );
-  };
-
-  const handleClickFocusAddNewItem = () => {
-    if (inputRefAddNewList.current !== null) {
-      inputRefAddNewList.current.focus();
-    }
   };
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
@@ -37,6 +30,11 @@ export const ToDoList = () => {
   const handleRemove = (targetIndex: number) => {
     const updatedIndexes = items.filter((item, index) => index !== targetIndex);
     setItems(updatedIndexes);
+
+    const updatedActiveIndexes = activeIndexes.filter(
+      (index) => index !== targetIndex
+    );
+    setActiveIndexes(updatedActiveIndexes);
   };
 
   return (
@@ -70,16 +68,7 @@ export const ToDoList = () => {
             </div>
           );
         })}
-
-      <h1 className="h1-add-new-item">add new</h1>
-      <div className="field" onClick={handleClickFocusAddNewItem}>
-        <input
-          type="text"
-          placeholder="Add a new to-do..."
-          ref={inputRefAddNewList}
-          onKeyDown={handleKeyDown}
-        />
-      </div>
+      <AddNewItem handleKeyDown={handleKeyDown} />
     </div>
   );
 };
